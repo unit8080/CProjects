3e5ec4b7fd60e721244a9d46e924246dd77ea3ef
Sanjeev Singh <sanjeev@Sanjeevs-MacBook-Air.local>
2024-04-08 13:07:35 -0700

    Upload Geeks

---
+++ Geeks/recurs-set4-1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void fun(int x)
+{
+  if(x > 0)
+  {
+     fun(--x);
+     printf("%d\t", x);
+     fun(--x);
+  }
+}
+ 
+int main()
+{
+  int a = 4;
+  fun(a);
+  getchar();
+  return 0;
+}
